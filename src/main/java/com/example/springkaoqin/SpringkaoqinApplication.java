93e53152fd62867fe71273143e17ce31fab9ee5b
zeimo、 <2878934647@qq.com>
2023-11-04 19:05:17 +0800

    Initial commit

---
+++ src/main/java/com/example/springkaoqin/SpringkaoqinApplication.java
@@ -0,0 +1,13 @@
+package com.example.springkaoqin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringkaoqinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringkaoqinApplication.class, args);
+    }
+
+}
